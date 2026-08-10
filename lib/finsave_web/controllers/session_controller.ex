4596770da056370bd84697cef76528150088c9a5
kiruxashafr <kirshafranov@gmail.com>
2026-08-10 19:34:16 +0300

    feat: session controller and gettext

---
+++ lib/finsave_web/controllers/session_controller.ex
@@ -0,0 +1,26 @@
+defmodule FinsaveWeb.SessionController do
+  use FinsaveWeb, :controller
+  alias Finsave.Identity
+
+  def create(conn, %{"user" => %{"login" => login, "password" => password}}) do
+    case Identity.authentificate(login, password) do
+      {:ok, account} ->
+        conn
+        |> put_session(:account_id, account.id)
+        |> put_flash(:info, gettext("Successfuly logged in!"))
+        |> redirect(to: "/dashboard")
+
+      {:error, :invalid_credentials} ->
+        conn
+        |> put_flash(:error, gettext("Authetification failed."))
+        |> redirect(to: "/auth/login")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> clear_session()
+    |> put_flash(:info, gettext("Logged out successfully."))
+    |> redirect(to: "/")
+  end
+end
